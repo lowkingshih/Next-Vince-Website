40db7ebc8f3ab3184c4d7c079c63eae13f8f19e9
loking23 <mongrock30102@gmail.com>
2023-06-09 10:52:06 +0800

    setting: App Router set up

---
+++ src/app/product-certification/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: '產品認證 | 巨東科技'
+}
+const ProductCertification = () => {
+  return (
+    <div>ProductCertification</div>
+  )
+}
+
+export default ProductCertification
---
+++ src/config.ts
@@ -0,0 +1,13 @@
+// 製作客製化 title
+export const products = {
+    'mcb1~63a': {
+    },
+    'hlc80~125a': {
+    }, 
+    'timer': {
+    }, 
+    'truncking': {
+    }, 
+    'add-on-button': {
+    }
+}
